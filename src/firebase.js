9ef0913bafc575367c5cff563bfa4b6ddf09896e
Vansh <you@example.com>
2022-04-12 19:40:32 +0530

    Fixes

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDl4JY2wL1PvYh5MfUXGWzOK2Ugvad4zrI",
+    authDomain: "smex-13c59.firebaseapp.com",
+    databaseURL: "https://smex-13c59-default-rtdb.firebaseio.com",
+    projectId: "smex-13c59",
+    storageBucket: "smex-13c59.appspot.com",
+    messagingSenderId: "806943212459",
+    appId: "1:806943212459:web:7587fca8dcbd2829f308ee",
+    measurementId: "G-XS12T2JZW8"
+  };
